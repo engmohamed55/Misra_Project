9dde059f690d9cb564547ebc562f71b85b7b152e
AbdElkader <mohamed.ahmed.05550@gmail.com>
2019-09-10 18:02:57 +0200

    KEYPAD files

--- KEYPAD.h
+++ KEYPAD.h
@@ -10,8 +10,23 @@
 #define KEYPAD_H_
 
 
-void KPD_Init(void);
-void KPD_ReadVal(unsigned char* ValuePtr);
+#define KPD_COL_PORT (PD)
+#define KPD_ROW_PORT (PD)
+#define KPD_COL_MASK ((u8)0x70)
+#define KPD_ROW_MASK ((u8)0x0f)
+#define KPD_COL_PIN_NUM (4u)
+#define KPD_ROW_PIN_NUM (0u)
+/**************************************************/
 
+
+void KPD_Init(void);
+
+
+void KPD_Init(void);
+void KPD_ReadVal(char* ValuePtr);
+void KPD_COL_READ(unsigned char* VALPTR);
+void KPD_ROW_WRITE(unsigned char DATA);
+void KPD_COL_INIT(void);
+void KPD_ROW_INIT(void);
 
 #endif /* KEYPAD_H_ */
